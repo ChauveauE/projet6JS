0c42c0c4439eebf3f4a01f2053cb23460acd2b43
Elodie CHAUVEAU <elochauveau@hotmail.fr>
2017-12-12 10:14:25 +0100

    project 6 JS

---
+++ JS.js
@@ -0,0 +1,16 @@
+function color(currentDiv)
+{
+	//alert("test");
+	document.body.style.backgroundColor ='red';
+	//document.body.style.backgroundColor ='blue';
+	//var last = currentDiv;		
+}
+function startOrStop()
+{
+	isStarted = !isStarted;
+
+	if (isStarted)
+	{
+		
+	}
+}

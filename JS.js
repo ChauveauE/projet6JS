dc462b6bb770ee24464641428ed3aada18444e4a
Elodie CHAUVEAU <elochauveau@hotmail.fr>
2017-12-15 11:07:33 +0100

    project 6 javascript

--- JS.js
+++ JS.js
@@ -1,16 +1,36 @@
-function color(currentDiv)
-{
-	//alert("test");
-	document.body.style.backgroundColor ='red';
-	//document.body.style.backgroundColor ='blue';
-	//var last = currentDiv;		
-}
+var interval;
+var isStarted = false;
+var index = 0;
+
 function startOrStop()
 {
 	isStarted = !isStarted;
 
 	if (isStarted)
 	{
-		
+		interval = setInterval(color,100);
+	}
+	else
+	{
+		clearInterval(interval);	
 	}
 }
+
+function color()
+{
+	document.body.style.backgroundColor = randomColor();		
+}
+
+function randomColor()
+{
+	var arrayColor = [
+		'red',
+		'blue',
+		'yellow', 
+		'green'
+	];
+	var color = arrayColor[index % arrayColor.length];
+	index++;
+	return color;
+}
+
